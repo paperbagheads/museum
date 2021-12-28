4bf8d4fb5dac1e9e66a5485c1f1640e2dfdf7138
anonape <18473081+anonape@users.noreply.github.com>
2021-12-27 21:09:01 -0500

    fixed large screen spacing issue

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,32 @@
 const shred = new Vue({
-    data() {
-      return { shredding: null, dropping: null }
+  data() {
+    return { shredding: null, dropping: null }
+  },
+  methods: {
+    shred() {
+      this.shredding = anime({ targets: '#original', height: 0, duration: 10000, easing: 'linear' })
+      this.dropping = anime({ targets: '#painting', translateY: '101%', duration: 10000, easing: 'linear' })
     },
-    methods: {
-      shred() {
-        this.shredding = anime({ targets: '#original', height: 0, duration: 10000, easing: 'linear' })
-        this.dropping = anime({ targets: '#painting', translateY: '101%', duration: 10000, easing: 'linear' })
-      },
-      artSelected(e) {
-        this.shredding.pause()
-        this.dropping.pause()
-        loadImage(e.target.files[0], canvas => {
-            let url = canvas.toDataURL('image/jpeg')
-            document.getElementById('original').style.backgroundImage = `url(${url})`
-            let elements = Array.from(document.getElementsByClassName('shred'))
-            elements.forEach(element => { element.style.backgroundImage = `url(${url})` })
-            document.getElementById('original').style.height    = '100%'
-            document.getElementById('painting').style.transform = 'translateY(0)'
-            this.shred()
-          }, { canvas: true, crop: true, maxHeight: 566, maxWidth: 392, orientation: true }
-        )
-      }
-    },
-  })
+    artSelected(e) {
+      this.shredding.pause()
+      this.dropping.pause()
+      loadImage(e.target.files[0], canvas => {
+          let url = canvas.toDataURL('image/jpeg')
+          document.getElementById('original').style.backgroundImage = `url(${url})`
+          let elements = Array.from(document.getElementsByClassName('shred'))
+          elements.forEach(element => { element.style.backgroundImage = `url(${url})` })
+          document.getElementById('original').style.height    = '100%'
+          document.getElementById('painting').style.transform = 'translateY(0)'
+          this.shred()
+        }, { canvas: true, crop: true, maxHeight: 566, maxWidth: 392, orientation: true }
+      )
+    }
+  },
+})
   
-  function shredImage() { shred.shred(); }
+function shredImage() { shred.shred(); }
+$.get('https://pbh-members.herokuapp.com/data', data => {
+  if (data.status) {
+    document.getElementById("pbhartpiece").src = data.data;
+  }
+});
